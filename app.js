e9e872cc8dec235e4ed0295c63ab0bd065edcc6f
AmurTigro <martiohusein27@gmail.com>
2023-09-25 16:27:33 +0700

    Change to make unblocking code for efeciency

--- app.js
+++ app.js
@@ -29,10 +29,11 @@ const server = http.createServer((req, res) => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
       console.log(parsedBody)
-      fs.writeFileSync('message.txt', message);
-      res.statusCode = 302;
-      res.setHeader('Location', '/home')
-      res.end()
+      fs.writeFile('message.txt', message, err => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/home')
+        res.end()
+      });
     })
   }
   res.setHeader("Content-Type", "text/html");
