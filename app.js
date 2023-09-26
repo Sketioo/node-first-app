90f692e7f4036b44d4b07b3c3177256569e533c8
AmurTigro <martiohusein27@gmail.com>
2023-09-26 18:22:27 +0700

    Delete routes.js and update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const http = require("http");
-const {requestHandler} = require("./routes")
 
-const server = http.createServer(requestHandler);
+const server = http.createServer();
 
 server.listen(3000);
